1f5672cc65d82c1d08d5f963cbe9fd3fcef6f302
Kator Agashua <katoragashua@outlook.com>
2022-10-07 11:01:35 +0100

    Debugging

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -33,9 +33,9 @@ const Contact = () => {
                 width="100"
                 height="80"
                 style={{ border: "0" }}
-                allowfullscreen=""
+                allowFullScreen=""
                 loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"
+                referrerPolicy="no-referrer-when-downgrade"
               ></iframe>
             </span>
           </div>
