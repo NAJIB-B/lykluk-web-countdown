6ff5301b7a967fda53536ae51279d478a9024710
Kator Agashua <katoragashua@outlook.com>
2022-10-07 10:57:53 +0100

    Embedded map

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -24,16 +24,31 @@ const Contact = () => {
             </ul>
             <h3>Our Address</h3>
             <span className="address">
-              <a href="">
+              <a href="https://goo.gl/maps/7AKSqeQsQNqnkVUS8" target="_blank">
                 <img src="./images/location.svg" alt="location icon" />
               </a>
               Suite F11, VGC Shopping Mall, Road 2, Victoria Garden City, Lagos.
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15857.872135727319!2d3.5453129608664544!3d6.46223464629185!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103bf73c539f14bf%3A0x4612f9e85fdacf83!2sVGC%20Shopping%20Mall%2C%20Victoria%20garden%20City%20106104%2C%20Lekki!5e0!3m2!1sen!2sng!4v1665135875077!5m2!1sen!2sng"
+                width="100"
+                height="80"
+                style={{ border: "0" }}
+                allowfullscreen=""
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"
+              ></iframe>
             </span>
           </div>
           <div className="contact-form">
             <form action="https://formsubmit.co/info@lykluk.com" method="post">
               <label htmlFor="email">Your Email</label>
-              <input type="email" name="email" id="email" required placeholder="Enter email"/>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                required
+                placeholder="Enter email"
+              />
               <label htmlFor="message">Message</label>
               <textarea
                 name="message"
