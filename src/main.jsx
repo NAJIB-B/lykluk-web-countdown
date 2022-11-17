85b32a8eb6fc48bf4ca172898678717b1e988642
Kator Agashua <katoragashua@outlook.com>
2022-11-17 08:40:29 +0100

    Added a favicon

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ContextProvider } from "./Context.jsx";
+import Favicon from "react-favicon";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ContextProvider>
     <React.StrictMode>
+      <Favicon url={"./images/lykluk-lite.svg"}/>
       <App />
     </React.StrictMode>
   </ContextProvider>
